151815e15430fe423e5bd2265c0956d93ece1ba0
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-12-17 09:52:07 +0100

    lint

--- packages/slack/src/plugins/thread-normalization.ts
+++ packages/slack/src/plugins/thread-normalization.ts
@@ -2,7 +2,7 @@ import { ApplicationPlugin, MessageResponse } from '@callstack/byorg-core';
 import { logger } from '@callstack/byorg-utils';
 
 /**
- * Slack plugin for normalizing messages coming from Slack [AI apps](https://api.slack.com/docs/apps/ai). 
+ * Slack plugin for normalizing messages coming from Slack [AI apps](https://api.slack.com/docs/apps/ai).
  */
 export const slackThreadNormalizerPlugin: ApplicationPlugin = {
   name: 'slack-thread-normalizer',
