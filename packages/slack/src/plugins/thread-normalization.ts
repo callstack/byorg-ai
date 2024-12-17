129b1b3e1adef322cf6ac33c4ffbea2a4968bec2
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-12-17 09:48:00 +0100

    Update packages/slack/src/plugins/thread-normalization.ts

--- packages/slack/src/plugins/thread-normalization.ts
+++ packages/slack/src/plugins/thread-normalization.ts
@@ -1,6 +1,9 @@
 import { ApplicationPlugin, MessageResponse } from '@callstack/byorg-core';
 import { logger } from '@callstack/byorg-utils';
 
+/**
+ * Slack plugin for normalizing messages coming from Slack [AI apps](https://api.slack.com/docs/apps/ai). 
+ */
 export const slackThreadNormalizerPlugin: ApplicationPlugin = {
   name: 'slack-thread-normalizer',
   middleware: async (context, next): Promise<MessageResponse> => {
